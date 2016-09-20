2d0fb63c42fd0ea2501d8db28519834194151e96
Grant Sutcliffe <grant@sutcliffes.ca>
2016-09-20 13:11:40 -0400

    Initial Commit

---
+++ PhotoViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoViewer
+//
+//  Created by Grant Sutcliffe on 2016-09-20.
+//  Copyright © 2016 gjsalot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
